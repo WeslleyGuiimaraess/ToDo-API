45f3d038a0a3bb7a592abe7f61ab8db96287b9eb
WeslleyGuiimaraess <weslley.toconoko@gmail.com>
2024-06-19 20:29:50 -0300

    inclui opção de adicionar nova tarefa e remover tarefa

---
+++ main.py
@@ -0,0 +1,93 @@
+import os
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+if not os.path.exists('db.txt'):
+    f = open('db.txt', 'w')
+    f.close()
+
+
+app = FastAPI()
+
+class Item(BaseModel):
+    identificador: int
+    descricao: str
+    status: bool
+
+
+@app.post("/item")
+def create(item: Item):
+
+    with open('db.txt', 'r') as f:
+        dados = f.readlines()
+        for line in dados:
+            campos_separados = line.split(',')
+            if int(campos_separados[0]) == int(item.identificador):
+                return {"erro" : "Mensagem de erro!"}
+
+    with open('db.txt', 'a') as f:
+        f.write(f"{item.identificador},{item.descricao},{item.status}\n")
+
+    return {"id": item.identificador, "descricao": item.descricao, "status": item.status}
+
+
+@app.get("/item")
+def list():
+
+    with open('db.txt', 'r') as f:
+        dados = f.readlines()
+
+        dados_em_lista = []
+        for line in dados:
+            campos_separados = line.split(',')
+            dados_em_lista.append({
+                "identificador":    str(campos_separados[0]),
+                "descricao":        str(campos_separados[1]),
+                "status":           str(campos_separados[2])
+            })
+
+        return dados_em_lista
+
+
+@app.get("/item/{item_id}")
+def get_one(item_id: int):
+
+    with open('db.txt', 'r') as f:
+        dados = f.readlines()
+
+        for line in dados:
+            campos_separados = line.split(',')
+            if int(campos_separados[0]) == item_id:
+                return {
+                        "identificador":    campos_separados[0],
+                        "descricao":        campos_separados[1],
+                        "status":           campos_separados[2]
+                }
+
+    return {"erro" : "Mensagem de erro!"}
+
+
+@app.put("/item")
+def nova_tarefa(item:Item):
+
+        with open('db.txt', 'a') as f:
+            f.write(f"{item.identificador, item.descricao, item.status}\n")
+        
+        return list()
+
+@app.put("/item")
+def remove_tarefa(item:Item, item_id: int):
+
+    with open('db.txt', 'r') as f:
+        dados = f.readlines()
+
+    for line in dados:
+            campos_separados = line.split(',')
+            if int(campos_separados[0]) == item_id:
+
+                with open('db.txt', 'a') as f:
+                    f.pop({item.identificador, item.descricao, item.status})
+    
+    
+
+
---
+++ tarefas.py
@@ -0,0 +1,5 @@
+class Tarefes():
+    def __init__(self,id,nome,status):
+        self.id = id
+        self.nome = nome
+        self.status = status
