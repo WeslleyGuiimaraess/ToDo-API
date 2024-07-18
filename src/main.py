e8ca14f4e0bde5db01516178da32f747fbb11d5e
WeslleyGuiimaraess <weslley.toconoko@gmail.com>
2024-07-17 21:43:28 -0300

    Arrumei os endpoints com postgres

--- src/main.py
+++ src/main.py
@@ -4,32 +4,27 @@ from pydantic import BaseModel
 import psycopg2
 
 
-if not os.path.exists('tarefas.localhost'):
-    try:
-        # Conectar ao banco de dados
-        with psycopg2.connect(
-            dbname="tarefas",
-            user="postgres",  # Substitua pelo usuário do seu banco de dados
-            password="password",  # Substitua pela senha do seu banco de dados
-            host="localhost",
-            port="5432"
-        ) as conn:
-            with conn.cursor() as cursor:
-                # Executar o comando de criação da tabela
-                cursor.execute("""
-                    CREATE TABLE tarefas (
-                        id INTEGER NOT NULL PRIMARY KEY,
-                        tarefa VARCHAR(255) NOT NULL,
-                        status BOOLEAN NOT NULL
-                    );
-                """)
-            
-            # Confirmar as mudanças no banco de dados
-            conn.commit()
-        
+try:
+    # Conectar ao banco de dados
+    with psycopg2.connect('postgres://postgres:password@db:5432/postgres') as conn:
+        with conn.cursor() as cursor:
+            # Executar o comando de criação da tabela
+            cursor.execute("""
+                CREATE TABLE tarefas (
+                    id INTEGER NOT NULL PRIMARY KEY,
+                    tarefa VARCHAR(255) NOT NULL,
+                    status BOOLEAN NOT NULL
+                );
+            """)
+
         print("Banco inicializado com sucesso.")
+        # Confirmar as mudanças no banco de dados
+        conn.commit()
     
-    except psycopg2.Error as e:
+    print("Banco inicializado com sucesso.")
+
+except psycopg2 as e:
+
         print(f"Erro ao inicializar banco de dados: {e}")
 
 app = FastAPI()
@@ -43,7 +38,7 @@ class Item(BaseModel):
 
 @app.post("/item")
 def create(item: Item) -> Item:
-    conn = psycopg2('postgres://postgres:password@localhost:5432/tarefas')
+    conn = psycopg2.connect('postgres://postgres:password@db:5432/postgres')
     cursor = conn.cursor()
 
     try:
@@ -51,7 +46,7 @@ def create(item: Item) -> Item:
             """
             INSERT INTO tarefas (id, tarefa, status)
             VALUES (
-                $1, $2, $3
+                %s, %s, %s
             );""", (item.identificador, item.descricao, item.status)
         )
     except Exception as e:
@@ -65,7 +60,7 @@ def create(item: Item) -> Item:
 @app.get("/item")
 def list() -> list[Item]:
 
-    conn = psycopg2('postgres://postgres:password@localhost:5432/tarefas')
+    conn = psycopg2.connect('postgres://postgres:password@db:5432/postgres')
     cursor = conn.cursor()
 
     try:
@@ -89,13 +84,13 @@ def list() -> list[Item]:
 @app.get("/item/{item_id}")
 def get_one(item_id: int) -> Item:
 
-    conn = psycopg2('postgres://postgres:password@localhost:5432/tarefas')
+    conn = psycopg2.connect('postgres://postgres:password@db:5432/postgres')
     cursor = conn.cursor()
 
     try:
         cursor.execute("""
-        SELECT * FROM tarefas WHERE id = $1
-                   """), (item_id)
+        SELECT * FROM tarefas WHERE id = %s;
+                   """, (item_id,))
 
         resultado = dict(zip(('identificador', 'descricao', 'status'), cursor.fetchone()))
     
@@ -110,15 +105,15 @@ def get_one(item_id: int) -> Item:
 @app.put("/item/{item_id}")
 def update(item_id: int, item:Item):
 
-    conn = psycopg2('postgres://postgres:password@localhost:5432/tarefas')
+    conn = psycopg2.connect('postgres://postgres:password@db:5432/postgres')
     cursor = conn.cursor()
   
     try:
         cursor.execute("""
         UPDATE tarefas
-        SET tarefa = $1, status = $2
-        WHERE id = $3
-        """), (item.descricao, item.status, item.identificador)
+        SET tarefa = %s, status = %s
+        WHERE id = %s;
+        """,  (item.descricao, item.status, item.identificador,))
     except Exception as e:
 
         return {'erro': str('Erro ao atualizar, verifique se os campos estão corretos')}
@@ -130,22 +125,22 @@ def update(item_id: int, item:Item):
 @app.delete("/item/{item_id}")
 def remove_tarefa(item_id: int):
 
-    conn = psycopg2('postgres://psycopg2:password@localhost:5432/tarefas')
+    conn = psycopg2.connect('postgres://postgres:password@db:5432/postgres')
     cursor = conn.cursor()
 
 
     try:
          cursor.execute("""
-            SELECT * FROM tarefas WHERE id = $1  
-            """), (item_id)
+            SELECT * FROM tarefas WHERE id = %s
+            """, (item_id,))
          
          resultado = dict(zip(('identificador', 'descricao', 'status'), cursor.fetchone()))
 
          if(resultado):
              
              cursor.execute("""
-                    DELETE FROM tarefas WHERE id = $1
-                            """), (item_id)
+                    DELETE FROM tarefas WHERE id = %s
+                            """, (item_id,))
              conn.commit()
              conn.close()
              return{'sucess': str('Tarefa apagada com sucesso')}
